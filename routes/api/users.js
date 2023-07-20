47e9c8c622a14d937614ad9ee04db6341b125004
Matt Ortner <matthewortner@gmail.com>
2023-07-19 20:21:36 -0400

    user can sign-up with email, password, username

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 const User = require("../../models/user-model");
 
@@ -13,56 +14,86 @@ router.get("/:id", (req, res) => {
 
 // @route POST api/users
 // @description Create user
-router.post("/sign-up", (req, res) => {
-  // check if email exists!!!!!!
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    isSignedIn: false,
-    favoriteProducts: [],
-    shoppingCart: [],
-    store: {
-      storeName: "",
-      products: [],
-    },
-  });
-  user
-    .save()
-    .then((savedUser) => res.send(savedUser))
-    .catch((err) => {
-      res.status(400).json({ error: "Unable to add account" });
-    });
+router.post("/sign-up", async (req, res) => {
+  const isUser = await User.findOne({ email: req.body.email });
+  try {
+    if (isUser) {
+      res.json({ body: "email exists" });
+    } else {
+      bcrypt.hash(req.body.password, 10, function (err, hash) {
+        const user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash,
+          isSignedIn: true,
+          favoriteProducts: [],
+          shoppingCart: [],
+          store: {
+            storeName: "",
+            products: [],
+          },
+        });
+        user
+          .save()
+          .then((savedUser) =>
+            res.send({
+              name: savedUser.name,
+              email: savedUser.email,
+              isSignedIn: savedUser.isSignedIn,
+              favoriteProducts: savedUser.favoriteProducts,
+              shoppingCart: savedUser.shoppingCart,
+              store: {
+                storeName: "",
+                products: savedUser.store.products,
+              },
+            })
+          )
+          .catch((err) => {
+            res.status(400).json({
+              error: "Unable to add account, please try again shortly.",
+            });
+            console.error("error", err);
+          });
+      });
+    }
+  } catch (error) {
+    res.status(500).send();
+  }
 });
 
 // @route POST api/users
 // @description Sign-In user
-router.post("/sign-in", (req, res) => {
-  User.findOneAndUpdate(
-    { email: req.body.email },
-    { isSignedIn: true },
-    {
-      returnDocument: "after",
+router.post("/sign-in", async (req, res) => {
+  const isUser = await User.findOne({ email: req.body.email });
+  try {
+    if (isUser) {
+      bcrypt.compare(
+        req.body.password,
+        isUser.password,
+        function (err, result) {
+          if (result) {
+            res.send({
+              name: isUser.name,
+              email: isUser.email,
+              isSignedIn: isUser.isSignedIn,
+              favoriteProducts: isUser.favoriteProducts,
+              shoppingCart: isUser.shoppingCart,
+              store: {
+                storeName: isUser.store.storeName,
+                products: isUser.store.products,
+              },
+            });
+          } else {
+            res.json({ body: "user does not exist" });
+          }
+        }
+      );
     }
-  )
-    .then((user) => {
-      res.send({
-        name: user.name,
-        email: user.email,
-        isSignedIn: user.isSignedIn,
-        favoriteProducts: user.favoriteProducts,
-        shoppingCart: user.shoppingCart,
-        store: {
-          storeName: user.store.storeName,
-          products: user.store.products,
-        },
-      });
-    })
-    .catch((err) => {
-      console.error("err", err);
-      console.error("err.config.data", err.config.data);
-      res.status(400).json({ error: "Unable to login" });
-    });
+  } catch (error) {
+    console.error("err", err);
+    console.error("err.config.data", err.config.data);
+    res.status(400).json({ error: "Unable to login" });
+  }
 });
 
 // @route PUT api/users
